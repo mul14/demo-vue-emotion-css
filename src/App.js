241c8233f3b408aaa90345631ef63b139c50a726
Mulia Nasution <mul14@users.noreply.github.com>
2021-09-10 13:07:32 +0700

    Update CSS

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import { css, cx } from '@emotion/css'
 export default function App() {
   return (
       <div className={css`
-        color: white;
-        background-color: red;
+        padding: 32px;
+        color: black;
+        background-color: DodgerBlue;
+        &:hover {
+          background-color: black;
+          color: yellow;
+          cursor: progress;
+        }
+        display: flex;
+        justify-content: center;
       `}>
         Hello World
       </div>
