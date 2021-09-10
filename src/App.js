967670d34746f9677e3a19818583ae50b67e9082
Mulia Nasution <mul14@users.noreply.github.com>
2021-09-10 13:02:07 +0700

    Change App.vue to App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { css, cx } from '@emotion/css'
+
+export default function App() {
+  return (
+      <div className={css`
+        color: white;
+        background-color: red;
+      `}>
+        Hello World
+      </div>
+  )
+}
